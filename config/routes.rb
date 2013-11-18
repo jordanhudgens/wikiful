dda8652be36048235d251d07986bd7f84ed1ded7
Jordan Hudgens <jordan@wow.com>
2013-11-18 13:26:29 -0600

    create controller for welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Wikiful::Application.routes.draw do
 
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
