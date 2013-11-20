883853f1a5cf8e70a2f850ffaa50707ee048e752
Jordan Hudgens <jordan@wow.com>
2013-11-20 13:00:03 -0600

    create Category model

---
+++ app/models/category.rb
@@ -0,0 +1,10 @@
+class Category < ActiveRecord::Base
+
+	belongs_to :user
+
+	has_many :article_categories
+	has_many :articles, through: :article_categories
+
+	validates :name, presence: true
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131120184129) do
+ActiveRecord::Schema.define(version: 20131120185121) do
 
   create_table "article_categories", force: true do |t|
     t.integer  "article_id"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20131120184129) do
   end
 
   create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
