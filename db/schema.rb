ff59a224c23489b470cdd0827dcfa06398e5031a
Jordan Hudgens <jordan@wow.com>
2013-11-20 12:48:01 -0600

    create new articlecategory model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131120182600) do
+ActiveRecord::Schema.define(version: 20131120184129) do
+
+  create_table "article_categories", force: true do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "articles", force: true do |t|
     t.string   "title"
