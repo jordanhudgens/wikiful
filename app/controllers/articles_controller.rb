8816c53a349ece2546f37cf03e03b2f5611b8cd5
Jordan Hudgens <jordan@wow.com>
2013-11-25 16:59:00 -0600

    build out new post controller and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,11 +12,29 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  # new method creates a new article
   def new
+    @article = Article.new
   end
 
+  # has user validation properties, shows the article
+  # if all of the validation requirements are met, if not it shows
+  # the form page again
   def create
+    @article = Article.new(article_params)
+    if @article.save
+        redirect_to @article
+    else
+        render "new"
+    end
   end
+
+# this is in place to establish the required
+# parameters for the new function to work
+private
+    def article_params
+        params.require(:article).permit(:title, :content, :category_ids => [])
+    end
 
 end
 
