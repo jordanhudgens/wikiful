e1d020801fba5f8cf19f3223487202bb6fbc4af7
Jordan Hudgens <jordan@wow.com>
2013-12-02 15:25:11 -0600

    fixed categories bug on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def index
       @recent_articles = Article.order(updated_at: :desc).limit(5)
+      @categories = Category.order(title: :desc)
   end
+
+  #def show
+     # @category = Category.find(params[:id])
+     # @article = Article.find(params[:id])
+#end
+
 end
