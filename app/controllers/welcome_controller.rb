b409362085f5b253646d7bc0b28ec62b97d5015e
Jordan Hudgens <jordan@wow.com>
2013-12-07 15:58:45 -0600

    formatted css

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-      @recent_articles = Article.order(updated_at: :desc).limit(5)
+      @recent_articles = Article.order(updated_at: :asc).limit(5)
   end
 
 end
